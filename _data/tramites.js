2f9a5bf55b5cf40f9c24ffd2b41d7b908be8a472
MarianoReyes <josemarianoreyesh02@gmail.com>
2023-09-22 10:52:39 -0600

    cambios en data

--- _data/tramites.js
+++ _data/tramites.js
@@ -56,16 +56,5 @@ module.exports = async function () {
   // Convert the response_final to JSON
   const jsonData = JSON.stringify(response_final, null, 2);
 
-  // Define the file path where you want to save the JSON file
-  const outputPath = 'jsons/tramites.json';
-
-  // Write the JSON data to the file
-  try {
-    fs.writeFileSync(outputPath, jsonData, 'utf8');
-    console.log(`JSON data successfully written to ${outputPath}`);
-  } catch (error) {
-    console.error('Error writing JSON data:', error);
-  }
-
   return response_final;
 };
